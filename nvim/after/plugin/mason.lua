2fc2c43c80fcaae95d779aaf8e78bd962ea4202b
Brendon Gonzalez <brendon@aurora.tech>
2024-04-11 15:14:03 +0000

    Add tsconfig setup

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -139,6 +139,23 @@ end)
 
 lsp.setup()
 
+require'lspconfig'.tsserver.setup{
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+        languages = {"javascript", "typescript", "vue"},
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "vue",
+  },
+}
+
 vim.diagnostic.config({
     virtual_text = true
 })
